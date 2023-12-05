d31be165efba89dea0b4f613044b368762c8b02e
valdijunior <valdi.ads@gmail.com>
2023-12-05 15:21:52 -0300

    Avançando em Props - aula 8

--- src/components/List.js
+++ src/components/List.js
@@ -4,9 +4,10 @@ function List(){
         <>
             <h1>Minha Lista</h1>
             <ul>
-                <Item marca="Ferrari"/>
-                <Item marca="Fiat"/>
-                <Item marca="Renault"/>
+                <Item marca="Ferrari" ano_lancamento={1985}/>
+                <Item marca="Fiat" ano_lancamento={1964}/>
+                <Item marca="Renault" ano_lancamento={1980}/>
+                <Item />
             </ul>
         </>
     );
