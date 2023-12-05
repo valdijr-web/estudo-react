76f84a5d19fa2832a0f34bff34978601624d9548
valdijunior <valdi.ads@gmail.com>
2023-12-04 23:17:26 -0300

    Utilizando React fragments - 7

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HelloWorld from './components/HellowWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
 import Frase from './components/Frase';
+import List from './components/List';
 
 function App() {
   const nome = "Maria";
@@ -20,6 +21,7 @@ function App() {
         profissao="Analista de Sistemas"
         foto="https://via.placeholder.com/150"
       />
+      <List />
     </div>
   );
 }
