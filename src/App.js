e5729d6891f5807c6b0622b95c50e93d69e66ff7
valdijunior <valdi.ads@gmail.com>
2023-12-04 22:12:25 -0300

    Entendendo JSX - Aula 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 
 function App() {
+
+  const name = 'Valdi'
+  const newName = name.toLocaleUpperCase()
+
+  function sum(a, b){
+    return a+b
+  }
+
+  const url = 'https://via.placeholder.com/150'
   return (
     <div className="App">
-      <h1>Olá React!</h1>
-      <p>Meu primeiro App</p>
+      <h2>Alterando o JSX</h2>
+      <p>Olá, {newName}</p>
+      <p>Soma: {2+2}</p>
+      <p>Soma com função: {sum(1,2)}</p>
+      <img src={url} alt="Minha Imagem"/>
     </div>
   );
 }
