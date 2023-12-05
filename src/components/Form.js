62628b7b2854f6f096b0da613b4f9d48f1a2de2a
valdijunior <valdi.ads@gmail.com>
2023-12-05 16:12:22 -0300

    hook useState na prática - aula 10

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,37 @@
+import { useState } from "react";
 function Form(){
 
     function cadastrarUsuario(e){
         e.preventDefault();
-        console.log("Cadastrou Usuário"+e.data);
+        console.log(name);
+        console.log(`Usuário ${name} foi cadastrado com a senha: ${password}`);
     }
+
+    const [name, setName] = useState();
+    const [password, setPassword] = useState();
     return(
         <div>
             <h1>Meu cadastro:</h1>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="Digite o seu nome"></input>
+                    <label htmlFor="name">Nome:</label>
+                    <input 
+                        type="text" 
+                        id="name" 
+                        name="name" 
+                        placeholder="Digite o seu nome"
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Senha:</label>
+                    <input 
+                        type="password" 
+                        id="password" 
+                        name="password" 
+                        placeholder="Digite a sua senha"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                 </div>
                 <div>
                     <input type="submit" value="Cadastrar"/>
